0c358dbc3965cc44d5d198ef7a562597c950e79d
Clebert Suconic <clebertsuconic@apache.org>
2023-02-16 09:20:58 -0500

    update queues names

--- create-cluster.sh
+++ create-cluster.sh
@@ -3,5 +3,5 @@ export BROKER=/Users/clebertsuconic/work/apache/activemq-artemis/artemis-distrib
 rm -rf node1
 rm -rf node2
 
-$BROKER/bin/artemis create --silent --force --user artemis --password artemis --role amq --port-offset 0 --allow-anonymous --no-autotune --verbose --name node1 --clustered --staticCluster tcp://localhost:61617 --max-hops 1 --queues testQueue node1
-$BROKER/bin/artemis create --silent --force --user artemis --password artemis --role amq --port-offset 0 --allow-anonymous --no-autotune --verbose --name node2 --clustered --staticCluster tcp://localhost:61616 --max-hops 1 --port-offset 1 --queues testQueue node2
+$BROKER/bin/artemis create --silent --force --user artemis --password artemis --role amq --port-offset 0 --allow-anonymous --no-autotune --verbose --name node1 --clustered --staticCluster tcp://localhost:61617 --max-hops 1 --queues amqp-demo-queue node1
+$BROKER/bin/artemis create --silent --force --user artemis --password artemis --role amq --port-offset 0 --allow-anonymous --no-autotune --verbose --name node2 --clustered --staticCluster tcp://localhost:61616 --max-hops 1 --port-offset 1 --queues amqp-demo-queue node2
