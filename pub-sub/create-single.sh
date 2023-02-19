5b0e614be6a5a307494959fb007e556f2b68758d
clebert <clebert.suconic@gmail.com>
2023-02-19 15:40:50 -0500

    local stuff

--- pub-sub/create-single.sh
+++ pub-sub/create-single.sh
@@ -4,4 +4,4 @@ rm -rf node1
 rm -rf node2
 
 #$BROKER/bin/artemis create --silent --force --user artemis --password artemis --role amq --port-offset 0 --allow-anonymous --no-autotune --verbose --name node1 --max-hops 1 --queues amqp-demo-queue node1
-$BROKER/bin/artemis create --silent --force --user artemis --password artemis --role amq --port-offset 0 --allow-anonymous --no-autotune --verbose --name node1 --max-hops 1 --queues amqp-demo-queue node1 --java-options "-agentpath:/Users/clebertsuconic/work/kevin/pub-sub/node1/bin/test.dll -javaagent:/Users/clebertsuconic/work/apache/check-leak/tick/target/check-leak-tick-0.8-SNAPSHOT.jar"
+$BROKER/bin/artemis create --silent --force --user artemis --password artemis --role amq --port-offset 0 --allow-anonymous --no-autotune --verbose --name node1 --max-hops 1 --queues amqp-demo-queue node1 --java-options "-agentpath:/home/clebert/work/tests-playground/pub-sub/test.dll  -javaagent:/home/clebert/work/check-leak/core/target/core-0.8-SNAPSHOT.jar=down=70;sleep=10000"
--- pub-sub/set-broker.sh
+++ pub-sub/set-broker.sh
@@ -1,2 +1,2 @@
 # set BROKER accordingly to your environment. Notice you don't include the /bin as part of the BROKER
-export BROKER=/Users/clebertsuconic/work/apache/activemq-artemis/artemis-distribution/target/apache-artemis-2.29.0-SNAPSHOT-bin/apache-artemis-2.29.0-SNAPSHOT
+export BROKER=/home/clebert/work/activemq-artemis/artemis-distribution/target/apache-artemis-2.29.0-SNAPSHOT-bin/apache-artemis-2.29.0-SNAPSHOT
