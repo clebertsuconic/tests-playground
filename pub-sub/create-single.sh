2d574b22262c8aebf5175f301ac57cef3b5e11b4
clebert <clebert.suconic@gmail.com>
2023-02-25 21:24:50 -0500

    set autotune

--- pub-sub/create-single.sh
+++ pub-sub/create-single.sh
@@ -9,4 +9,4 @@ java -jar $AGENT test.dll
 #$BROKER/bin/artemis create --silent --force --user artemis --password artemis --role amq --port-offset 0 --allow-anonymous --no-autotune --verbose --name node1 --max-hops 1 --queues amqp-demo-queue node1
 #$BROKER/bin/artemis create --silent --force --user artemis --password artemis --role amq --port-offset 0 --allow-anonymous --no-autotune --verbose --name node1 --max-hops 1 --queues amqp-demo-queue node1 --java-options "-agentpath:/Users/clebertsuconic/work/kevin/pub-sub/test.dll -javaagent:/Users/clebertsuconic/work/apache/check-leak/core/target/core-0.8-SNAPSHOT.jar=output=sleep=1000"
 
-$BROKER/bin/artemis create --silent --force --user artemis --password artemis --role amq --port-offset 0 --allow-anonymous --no-autotune --verbose --name node1 --max-hops 1 --queues amqp-demo-queue node1 --java-options "$AGENT_OPTIONS"
+$BROKER/bin/artemis create --silent --force --user artemis --password artemis --role amq --port-offset 0 --allow-anonymous --name node1 --max-hops 1 --queues amqp-demo-queue node1 --java-options "$AGENT_OPTIONS"
