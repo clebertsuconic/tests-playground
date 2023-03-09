093c56c1e3ae4fa75a040c972b84b2b6f6c66959
Angelo Galvao <angelogalvao@gmail.com>
2023-03-09 15:10:28 -0500

    Improved federated test snceario

--- pub-sub/set-broker.sh
+++ pub-sub/set-broker.sh
@@ -1,5 +1,5 @@
 # set BROKER accordingly to your environment. Notice you don't include the /bin as part of the BROKER
-export BROKER=/Users/asouza/redhat/middleware/amq/amq-broker-7.10.2
+export BROKER=/Users/asouza/redhat/middleware/amq/amq-broker-7.10.2-PATCH-3248
 export AGENT=/Users/clebertsuconic/work/apache/check-leak/check-leak/target/check-leak-0.8-SNAPSHOT.jar
 export CURRENT_DIR=`pwd`
 
